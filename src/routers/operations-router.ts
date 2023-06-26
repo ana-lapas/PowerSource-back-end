d58b8296e3521bf78ddcb1fcc2c728ee3dbaa5c5
ana-lapas <anapaulalapas@gmail.com>
2023-06-26 16:13:55 -0400

    Remove post Operation

--- src/routers/operations-router.ts
+++ src/routers/operations-router.ts
@@ -1,5 +1,5 @@
 import { checkJWT } from "../middleware/user-middleware";
-import { insertOperation, loadOperations } from "../controller/operations-controller";
+import { loadOperations } from "../controller/operations-controller";
 import express from "express";
 import { findOperationsByClientId } from "../repositories/operations-repositories";
 import { getOperations } from "../service/operations-service";
@@ -7,6 +7,5 @@ import { getOperations } from "../service/operations-service";
 const operationsRouter = express.Router();
 
 operationsRouter.get('/', checkJWT, loadOperations, getOperations, findOperationsByClientId);
-operationsRouter.post('/create', checkJWT, insertOperation, );
 
 export { operationsRouter };
